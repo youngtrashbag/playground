9b2d26ca901323f38d4d780fb82a2a97fff078d6
youngtrashbag <yuule.smeker@gmail.com>
2020-01-31 12:17:44 +0100

    add first revision of opencv project

---
+++ opencv-display-image/src/header.h
@@ -0,0 +1,17 @@
+//global includes
+#include <iostream>
+
+//opencv includes
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+//defining different error codes
+#ifndef NO_ARG_ERR
+#define NO_ARG_ERR 2
+#endif //NO_ARG_ERR
+
+#ifndef IMG_EMPTY_ERR
+#define IMG_EMPTY_ERR 3
+#endif //IMG_EMPTY_ERR
+
---
+++ opencv-display-image/src/open.cpp
@@ -0,0 +1,37 @@
+#include "header.h"
+
+int main(int argc, char* argv[])
+{
+	if(argc <= 1)
+	{
+		//no filepath as argument
+		std::cout << "Please enter a filepath as argument" << std::endl;
+		exit(NO_ARG_ERR);
+	}
+
+	//set filepath/name as variable
+	char[] filePath = argv[1];
+
+	Mat image;
+
+	//read the file from the command line argument
+	image = imread(samples::findFile(filePath), IMREAD_COLOR);
+
+	if(image.empty())
+	{
+		std::cout << "Image is Empty" << std::endl;
+		exit(IMG_EMTPY_ERR);
+	}
+
+	//create a window
+	namedWindow("Image: " + filePath, WINDOW_AUTOSIZE);
+
+	imshow("test", image);
+
+	//wait for a keystroke
+	waitKey(0);
+
+	//conventional
+	return 0;
+}
+
