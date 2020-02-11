63fa681c23e29f278697decf7f5eee183a1cba1e
youngtrashbag <yuule.smeker@gmail.com>
2020-02-11 10:43:57 +0100

    fix formatting a little

--- random/stats/src/func.cpp
+++ random/stats/src/func.cpp
@@ -17,7 +17,7 @@ unsigned int* CreateRandomNumbers(unsigned int iterations, unsigned int maxValue
 {
 	unsigned int* numbers = new unsigned int[iterations]();
 
-	for(int i=0; i<iterations; i++)
+	for(int i = 0; i < iterations; i++)
 	{
 		numbers[i] = randombytes_uniform(maxValue+1);
 	}
@@ -30,10 +30,10 @@ int* GetDistribution(unsigned int* numberArray, unsigned int maxValue)
 {
 	int* dist = new int[sizeof(numberArray)]();
 
-	//iterate trough number array
-	for(int i=0; i<sizeof(numberArray)/sizeof(numberArray[0]); i++)
+	//iterate trough number array (the sizeof operation is the number of iterations)
+	for(int i = 0; i < sizeof(numberArray) / sizeof(numberArray[0]); i++)
 	{
-		for(int d=0; d<maxValue+1; d++)
+		for(int d = 0; d < maxValue + 1; d++)
 		{
 			//look, if the number "d" in maxValue matches
 			//the randomly generated number
@@ -52,7 +52,7 @@ int* GetDistribution(unsigned int* numberArray, unsigned int maxValue)
 //i know no other way than to pass the maxValue variable everytime
 void DisplayDistribution(int* distArray, unsigned int maxValue)
 {
-	for(int i=0; i<maxValue+1; i++)
+	for(int i = 0; i < maxValue + 1; i++)
 	{
 		std::cout << "Num[" << i << "]\t";
 		std::cout << "Gen[" << distArray[i] << "]"<< std::endl;
