de6a0643fd6041223c3d1a1ee4cc4fbc0661c453
Elias Albisser <elias.albisser@siemens.com>
2020-11-24 10:40:03 +0100

    feat: implement pig latin algorithm
    yesh

--- rust/pig-latin/src/main.rs
+++ rust/pig-latin/src/main.rs
@@ -6,41 +6,99 @@ fn main() {
     // 1. the first consonant is moved at the end, and -ay is added (first -> irst-fay)
     // 2. words that start with a vowel have -hay appended (apple -> apple-hay)
     // 3. if a word starts with the 'th' digraph both letters will be appended (think -> ink-thay)
+
+    //add easily readable option (irst-fay/irstfay)
+    let readable: bool = true;
     
-    let vowels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u'];
+    let words: Vec<&str> = phrase.split(' ').collect();
+    let mut char_words: Vec<Vec<char>> = vec![vec![]];
+
+    //removing the first empty element
+    char_words.pop();
+
+    for &x in words.iter() {
+        char_words.push(x.to_string().chars().collect());
+    }
+
+    // new variable, because of mutability
+    let mut new_words: Vec<String> = vec![];
+
+    // determining, if the first letter is vowel or consonant
+    for word in char_words.iter() {
+        let mut new_word = word.to_vec();
+
+        if word[0] == 't' && word[1] == 'h' {
+            let first: char = new_word[0];
+            let second: char = new_word[1];
+            new_word.remove(0);
+            // remove twice at index 0, because content will automatically shift
+            new_word.remove(0);
+            
+            if readable {
+                new_word.push('-');
+            }
+
+            new_word.push(first);
+            new_word.push(second);
+            new_word.push('a');
+            new_word.push('y');
+
+        } else if is_consonant(word[0]) {
+            let first: char = new_word[0];
+            new_word.remove(0);
+
+            if readable {
+                new_word.push('-');
+            }
+
+            new_word.push(first);
+            new_word.push('a');
+            new_word.push('y');
+        } else if is_vowel(word[0]) {
+            if readable {
+                new_word.push('-');
+            }
+
+            new_word.push('h');
+            new_word.push('a');
+            new_word.push('y');
+        }
+
+        new_word.push(' ');
+        new_words.push(new_word.into_iter().collect());
+    }
+
+    // converting the vector to strings
+    let pig_latin = &new_words.into_iter().collect::<String>();
+    println!("phrase:\n\t{}\n\npig latin:\n\t{}", phrase, pig_latin);
+}
+
+fn is_consonant(letter: char) -> bool {
     let consonants: Vec<char> = vec!['b', 'c', 'd', 'f', 'g',
     'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v',
     'w', 'x', 'y', 'z', ];
 
-    //let alphabet: Vec<char> = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let mut yes: bool = false;
 
-    let mut words: Vec<String> = vec![String::from("")];
-
-    for word in phrase.split_ascii_whitespace() {
-        for letter in &vowels {
-            // word starts with a vowel
-            if word.chars().next() == Some(*letter) {
-                let mut new: String = String::from(word.trim_start());
-                new = format!("{}-{}ay", new, letter);
-                words.push(new);
-            }
-        }
-        for letter in &consonants {
-            // word starts with digraph (th)
-            if word.chars().next() == Some('t') && word.chars().next() == Some('h') {
-                let mut new: String = String::from(word.trim_start());
-                new = format!("{}-thay", new);
-                words.push(new);
-            }
-            else if word.chars().next() == Some(*letter) {
-                let mut new: String = String::from(word.trim_start());
-                new = format!("{}-{}ay", new, letter);
-                words.push(new);
-            }
+    for cons in consonants.iter() {
+        if letter == *cons {
+            yes = true;
         }
     }
 
-    println!("phrase: {}\npig lating: {:?}", phrase, words);
+    yes
+}
 
-    // fucc this, i need to implement this later, im not good enough yet ..
+fn is_vowel(letter: char) -> bool {
+    let vowels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u'];
+
+    let mut yes: bool = false;
+
+    for vow in vowels.iter() {
+        if letter == *vow {
+            yes = true;
+        }
+    }
+
+    yes
 }
