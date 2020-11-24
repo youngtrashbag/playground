b929302adca704654ff8c8b18f82c5a4965858c8
Elias Albisser <elias.albisser@siemens.com>
2020-11-24 11:39:52 +0100

    chore: put pig latinify process into function

--- rust/pig-latin/src/main.rs
+++ rust/pig-latin/src/main.rs
@@ -1,23 +1,41 @@
+// rules
+// 1. the first consonant is moved at the end, and -ay is added (first -> irst-fay)
+// 2. words that start with a vowel have -hay appended (apple -> apple-hay)
+// 3. if a word starts with the 'th' digraph both letters will be appended (think -> ink-thay)
+
+use std::env;
+
+// constants (I'm using them similarly as in C/C++)
+const EASY_READABILITY: bool = true;
+
 fn main() {
     //let phrase = String::from("What do you think, Pam?");
-    let phrase = String::from("hello world test thest apple");
+    let mut phrase = String::from("hello world test thest apple");
 
-    // rules
-    // 1. the first consonant is moved at the end, and -ay is added (first -> irst-fay)
-    // 2. words that start with a vowel have -hay appended (apple -> apple-hay)
-    // 3. if a word starts with the 'th' digraph both letters will be appended (think -> ink-thay)
+    /*
+    // parse arguments
+    let mut args: Vec<String> = env::args().collect();
 
-    //add easily readable option (irst-fay/irstfay)
-    let readable: bool = true;
+    // one argument passed (first arg is always program file/name)
+    if args.len() >= 2 {
+        phrase = "".to_string();
+        args.remove(0);
+
+        for element in args {
+            phrase += &element;
+            phrase += " ";
+        }
+    }
+    */
     
     let words: Vec<&str> = phrase.split(' ').collect();
     let mut char_words: Vec<Vec<char>> = vec![vec![]];
 
-    //removing the first empty element
+    //removing the first empty element of vector
     char_words.pop();
 
-    for &x in words.iter() {
-        char_words.push(x.to_string().chars().collect());
+    for &word in words.iter() {
+        char_words.push(word.to_string().chars().collect());
     }
 
     // new variable, because of mutability
@@ -25,52 +43,59 @@ fn main() {
 
     // determining, if the first letter is vowel or consonant
     for word in char_words.iter() {
-        let mut new_word = word.to_vec();
+        //let copy_of_vec = word.to_vec();
+        let mut pig_latinified_word = pig_latinify(word.to_vec());
 
-        if word[0] == 't' && word[1] == 'h' {
-            let first: char = new_word[0];
-            let second: char = new_word[1];
-            new_word.remove(0);
-            // remove twice at index 0, because content will automatically shift
-            new_word.remove(0);
-            
-            if readable {
-                new_word.push('-');
-            }
-
-            new_word.push(first);
-            new_word.push(second);
-            new_word.push('a');
-            new_word.push('y');
-
-        } else if is_consonant(word[0]) {
-            let first: char = new_word[0];
-            new_word.remove(0);
-
-            if readable {
-                new_word.push('-');
-            }
-
-            new_word.push(first);
-            new_word.push('a');
-            new_word.push('y');
-        } else if is_vowel(word[0]) {
-            if readable {
-                new_word.push('-');
-            }
-
-            new_word.push('h');
-            new_word.push('a');
-            new_word.push('y');
-        }
-
-        new_word.push(' ');
-        new_words.push(new_word.into_iter().collect());
+        pig_latinified_word.push(' ');
+        new_words.push(pig_latinified_word.into_iter().collect());
     }
 
     // converting the vector to strings
     let pig_latin = &new_words.into_iter().collect::<String>();
     println!("phrase:\n\t{}\n\npig latin:\n\t{}", phrase, pig_latin);
+}
+
+fn pig_latinify(word: Vec<char>) -> Vec<char>{
+    let mut new_word = word.to_vec();
+
+    if word[1] == 'h' {
+        let first: char = new_word[0];
+        let second: char = new_word[1];
+        new_word.remove(0);
+        // remove twice at index 0, because content will automatically shift
+        new_word.remove(0);
+        
+        if EASY_READABILITY {
+            new_word.push('-');
+        }
+
+        new_word.push(first);
+        new_word.push(second);
+        new_word.push('a');
+        new_word.push('y');
+
+    } else if is_consonant(word[0]) {
+        let first: char = word[0];
+        new_word.remove(0);
+
+        if EASY_READABILITY {
+            new_word.push('-');
+        }
+
+        new_word.push(first);
+        new_word.push('a');
+        new_word.push('y');
+    } else if is_vowel(word[0]) {
+        if EASY_READABILITY {
+            new_word.push('-');
+        }
+
+        new_word.push('h');
+        new_word.push('a');
+        new_word.push('y');
+    }
+
+    new_word
 }
 
 fn is_consonant(letter: char) -> bool {
