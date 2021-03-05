a0fc733c5ec3488a210358dc1ff66fbc2e5b9b28
youngtrashbag <yuule.smeker@gmail.com>
2021-03-05 13:55:05 +0100

    chore: add option to edit title

--- web/react-test/src/App.js
+++ web/react-test/src/App.js
@@ -2,6 +2,17 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
+const selection = [
+  {
+    title: "low res hounds....",
+    secret: "kiffä hound"
+  },
+  {
+    title: "!",
+    secret: "chef hound"
+  }
+]
+
 const HoundImage = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState();
@@ -31,16 +42,24 @@ const HoundImage = () => {
 }
 
 const App = () => {
-  // change document attribtues
+  const [metadata, setMetadata] = useState(selection[0]);
+  const [title, setTitle] = useState(metadata.title);
+
   useEffect(() => {
-    document.title = "low res hounds"
-    // funny secret meme
-    window.sessionStorage.setItem("secret", "kiffä hound");
-  });
+    document.title = title
+
+    window.sessionStorage.setItem("secret", metadata.secret);
+  }, [])
 
   return (
     <div className="container">
-      <h1>low res hounds....</h1>
+      <h1>{title}</h1>
+      <input
+        id="newTitle"
+        type="text"
+        placeholder="change title of this page"
+        onChange={ () => {setTitle(document.getElementById("newTitle").value)} }
+      />
       <div className="subContainer">
         <HoundImage />
       </div>
